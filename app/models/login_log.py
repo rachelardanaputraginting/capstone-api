0fcd87da3b1e9fa2731d738a427f37e4e1c6e838
rachelardanaputraginting <rachelardanaputraginting@gmail.com>
2024-11-22 18:49:07 +0700

    feat: update login logs

--- app/models/login_log.py
+++ app/models/login_log.py
@@ -1,30 +1,47 @@
+from flask import current_app
+import logging
 from app.extensions import db
 from datetime import datetime
+from utils.datetime import get_current_time_in_timezone
 
-class LoginLog(db.Model) :
-    
-  __tablename__ = 'login_logs'
+class LoginLog(db.Model):
+    __tablename__ = 'login_logs'
   
-  id = db.Column(db.Integer, primary_key=True)
-  token_identifier = db.Column(db.Text, nullable=False)
-  destroy_at = db.Column(db.DateTime, nullable=True)
-  created_at = db.Column(db.DateTime, nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    token_identifier = db.Column(db.Text, nullable=False)
+    destroy_at = db.Column(db.DateTime, nullable=True)
+    created_at = db.Column(db.DateTime, nullable=False)
 
-  def __init__(self, token_identifier: str) :
-    self.token_identifier = token_identifier
-    self.created_at = datetime.now()
-    self.save()
-      
-  def destroy(self) :
-    db.session.delete(self)
-    db.session.commit()
-  
-  def update(self, fields: dict = {}) :
-    for field in fields :
-        if fields[field] :
-          setattr(self, field, fields[field])
-    self.save()
+    def __init__(self, token_identifier: str):
+        self.token_identifier = token_identifier
+        self.created_at = get_current_time_in_timezone('Asia/Jakarta') 
+        self.save()
 
-  def save(self) :
-    db.session.add(self)
-    db.session.commit()
+    def save(self):
+        try:
+            db.session.add(self)
+            db.session.commit()
+        except Exception as e:
+            current_app.logger.error(f"Error while saving LoginLog: {e}")
+            db.session.rollback()  # Rollback if there is an error
+            raise e
+
+    def destroy(self):
+        try:
+            db.session.delete(self)
+            db.session.commit()
+        except Exception as e:
+            current_app.logger.error(f"Error while deleting LoginLog: {e}")
+            db.session.rollback()
+            raise e
+
+    def update(self, fields: dict = {}):
+        try:
+            for field in fields:
+                if fields[field]:
+                    setattr(self, field, fields[field])
+            self.save()
+        except Exception as e:
+            current_app.logger.error(f"Error while updating LoginLog: {e}")
+            db.session.rollback()
+            raise e
