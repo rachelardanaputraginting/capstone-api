067ee8f0c95c37a408a7e99855c7c839b4886a46
rachelardanaputraginting <rachelardanaputraginting@gmail.com>
2024-11-23 12:29:25 +0700

    chore: move file login dan register schema and delete unnecessary files

--- app/schemas/login_schema.py
+++
@@ -1,5 +0,0 @@
-from marshmallow import Schema, fields, validate
-
-class LoginSchema(Schema):
-   email = fields.Email(required=True, validate=[validate.Length(max=50)])
-   password = fields.String(required=True, validate=[validate.Length(min=8)])
